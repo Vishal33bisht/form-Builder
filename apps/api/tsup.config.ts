15ed47e863b8c37ccd8922899f86eecb45de4cbb
vishal <bishtv773@gmail.com>
2026-05-27 22:15:38 +0530

    added external to prevent bundling  native

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["./src/index.ts"],
-  noExternal: [/(^@repo\/.*)/],
+  noExternal: ["@repo/logger", "@repo/trpc"],
+  external: ["pg", "pg-native"],
   splitting: false,
   bundle: true,
   outDir: "./dist",
